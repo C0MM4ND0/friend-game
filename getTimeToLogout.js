f4d694f2d5ef51ce084552aa38e7e4e653d6d676
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-04-23 11:32:48 -0400

    small update to logout trigger

--- getTimeToLogout.js
+++ getTimeToLogout.js
@@ -2,35 +2,39 @@ $(document).ready(main);
 
 function main(){
 
+    var exp;
+    var timeLeft;
+    var secCounter;
 
 
-    setInterval(function(){
-        $.ajax({
-                type: "get",
-                action: "get",
-                url: "/time-to-logout",
-                success: function(result){                      // when logged out, force screen refresh
+    $.ajax({
+        type: "get",
+        action: "get",
+        url: "/time-to-logout",
+        success: function(result){                      // when logged out, force screen refresh
+            exp = new Date(result);
+            timeLeft = Math.floor((exp - Date.now())/1000);
+            secCounter = timeLeft;
+        }
+    })
 
 
-                    exp = new Date(result);
-                    var timeLeft = Math.floor((exp - Date.now())/1000);
-                    console.log("got result: " + timeLeft);
-
-                    if(isNaN(timeLeft)){
-                        timeLeft = "Logged out";
-
-                        if($("#time-left").text() != "Logged out"){
-                            console.log("reloading!");
-                            window.location.reload();
-                        }
-                    }
-
-                    $("#time-left").text(timeLeft);
-
-
-                }
-            })
+    var timer = setInterval(function(){ 
+        console.log("tick");
+        if(secCounter < 1 || isNaN(secCounter)){                             // if we're logging out, stop counting
+            console.log("clearing timer!");
+            clearInterval(timer);
+            if($("#time-left").text() != "Logged out"){
+                console.log("reloading!");
+                window.location.reload();
+            } 
+        } else {
+            $("#time-left").text(secCounter);               // update counter on the page
+            secCounter--;                                   // count down one second
+        }
 
     }, 1000)
+
+
 
 }
